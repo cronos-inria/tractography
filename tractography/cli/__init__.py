ca41b7ae9177597bdfa585ef0959028cb0256dc6
Samuel Deslauriers-Gauthier <samuel.deslauriers-gauthier@inria.fr>
2024-12-19 16:25:05 +0100

    feature: make CLI auto detect subcommands
    The CLI now does a bit of introspection to detect the available
    subcommands which are located in the `tg.cli` package.

--- tractography/cli/__init__.py
+++ tractography/cli/__init__.py
@@ -1,83 +1,36 @@
-"""Entry-point for the Command Line Interface
-
-This module implements the main CLI for the tractography package. It
-allows the user to perform tractography from files.
-
-"""
-
 import argparse
-from enum import Enum
-from pathlib import Path
+import pkgutil
 
-from .. import deterministic
+import tractography
 
 
-_DESCRIPTION = """
-Perform diffusion magnetic resonance imaging tractography.
-"""
-
-_ALGORITHM_HELP = """
-the algorithm used for tractography
-"""
-
-_IMAGE_HELP = """
-the filename of the image on which to perform tractography
-"""
-
-_TRACTOGRAM_HELP = """
-the filename of the generated tractogram
-"""
-
-_N_STREAMLINES_HELP = """
-the number of streamlines to generate
-"""
-
-_N_STEPS = """
-the number of steps for each streamline
-"""
-
-_MAX_ANGLE = """
-the maximum angle between consecutive steps
-"""
-
-_STEP_SIZE = """
-the size of each step
+DESCRIPTION = """\
 """
 
 
-class Algorithm(Enum):
-    """The various tractography algorithms"""
+def parse_arguments():
 
-    DETERMINISTIC = "det"
-    PROBABILISTIC = "prob"
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    subparsers = parser.add_subparsers()
+    subparsers.required = True
+    subparsers.dest = "subcommand"
 
-    def __str__(self):
-        return self.value
+    # Load all the sub commands from the streamlines.cli.commands package
+    # dynamically.
+    package = tractography.cli
+    prefix = package.__name__ + "."
+    for _, name, _ in pkgutil.iter_modules(package.__path__, prefix):
+        module = __import__(name, fromlist=["nothing"])
+        module.add_parser(subparsers)
+
+    return parser.parse_args()
 
 
 def main():
-    """Entry-point of the tractography CLI"""
-    args = parse_args()
-    print(args)
 
-
-def parse_args():
-    """Parse CLI arguments"""
-
-    parser = argparse.ArgumentParser(description=_DESCRIPTION)
-    parser.add_argument(
-        "algorithm", type=Algorithm, choices=list(Algorithm), help=_ALGORITHM_HELP
-    )
-    parser.add_argument("image", type=Path, help=_IMAGE_HELP)
-    parser.add_argument("output", type=Path, help=_TRACTOGRAM_HELP)
-    parser.add_argument(
-        "--n_streamlines", type=int, default=10000, help=_N_STREAMLINES_HELP
-    )
-    parser.add_argument("--n_steps", type=int, default=100, help=_N_STEPS)
-    parser.add_argument("--max_angle", type=float, default=45, help=_MAX_ANGLE)
-    parser.add_argument("--step_size", type=float, default=0.1, help=_STEP_SIZE)
-
-    return parser.parse_args()
+    args = parse_arguments()
+    parameters = {k: v for k, v in vars(args).items() if k != "func"}
+    args.func(**parameters)
 
 
 if __name__ == "__main__":
