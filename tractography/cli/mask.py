dfe2f335b0d7927e06900cf6baa474d8af438dd2
Samuel Deslauriers-Gauthier <samuel.deslauriers-gauthier@inria.fr>
2024-12-19 16:26:48 +0100

    feature: add `mask` CLI subcommand
    This commit adds a `mask` subcommand to the CLI which generates
    masks suitable for tractography. Fow now, masks can only be
    generated from aparc+aseg images.

---
+++ tractography/cli/mask.py
@@ -0,0 +1,45 @@
+"""Implements the 'tractography mask' CLI"""
+
+from pathlib import Path
+
+import nibabel as nib
+import numpy as np
+
+
+_DESCRIPTION = """
+Generate a tractography mask from a brain parcellation and segmentation
+"""
+
+_HELP = """
+generate a tractography mask
+"""
+
+_IMAGE_PATH_HELP = """
+the path to the image used to generate the mask
+"""
+
+_MASK_PATH_HELP = """
+the path to the generated mask
+"""
+
+_WM_LABELS = (2, 16, 28, 41, 60, 251, 252, 253, 254, 255)
+
+
+def main(image_path: Path, mask_path: Path, **kwargs: dict):
+    """Generate a mask from image data"""
+    mask_nii = nib.load(image_path)
+    mask_data = mask_nii.get_fdata()
+    mask = np.isin(mask_data, _WM_LABELS).astype(np.uint8)
+    nib.save(nib.Nifti1Image(mask, mask_nii.affine), mask_path)
+
+
+def add_parser(subparsers):
+    """Add the surparser for the mask subcommand"""
+    subparser = subparsers.add_parser("mask", description=_DESCRIPTION, help=_HELP)
+    subparser.add_argument("image_path", type=Path, help=_IMAGE_PATH_HELP)
+    subparser.add_argument("mask_path", type=str, help=_MASK_PATH_HELP)
+    subparser.set_defaults(func=main)
+
+
+if __name__ == "__main__":
+    main()
