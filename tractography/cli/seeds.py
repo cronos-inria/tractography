fc765f725c74e150048c6d7ebe5911adba8accc9
Samuel Deslauriers-Gauthier <samuel.deslauriers-gauthier@inria.fr>
2024-12-19 16:29:11 +0100

    feature: add a `seeds` subcommand for the CLI
    This commit adds a `seeds` subcommand to the CLI which generates
    tractography seeds from a FreeSurfer surface (e.g. lh.white).

---
+++ tractography/cli/seeds.py
@@ -0,0 +1,49 @@
+"""Implements the 'tractography mask' CLI"""
+
+from pathlib import Path
+
+import nimesh
+import numpy as np
+
+import tractography as tg
+
+
+_DESCRIPTION = """
+Generate tractography seeds from a triangular surface
+"""
+
+_HELP = """
+generate tractography seeds
+"""
+
+_SURFACE_PATH_HELP = """
+the path to the surface used to generate the seeds
+"""
+
+_N_SEEDS_HELP = """
+the number of seeds to generate
+"""
+
+_SEEDS_PATH_HELP = """
+the path to the generated seeds
+"""
+
+
+def main(surface_path: Path, n_seeds: int, seeds_path: Path, **kwargs: dict):
+    """Generate a seeds from the surface"""
+    surface = nimesh.io.load(surface_path, hemisphere="lh", surface="white")
+    seeds = tg.seeds.from_surface(surface, n_seeds)
+    tg.seeds.save(seeds_path, seeds)
+
+
+def add_parser(subparsers):
+    """Add the surparser for the seeds subcommand"""
+    subparser = subparsers.add_parser("seeds", description=_DESCRIPTION, help=_HELP)
+    subparser.add_argument("surface_path", type=Path, help=_SURFACE_PATH_HELP)
+    subparser.add_argument("n_seeds", type=int, help=_N_SEEDS_HELP)
+    subparser.add_argument("seeds_path", type=str, help=_SEEDS_PATH_HELP)
+    subparser.set_defaults(func=main)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tractography/seeds.py
@@ -0,0 +1,83 @@
+"""Generate seeds for tractography"""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+import nimesh
+import numpy as np
+import numpy.typing as npt
+
+
+@dataclass
+class Seed:
+    """A single streamline seed"""
+
+    location: npt.ArrayLike
+    orientation: npt.ArrayLike
+
+
+def from_surface(surface: nimesh.Mesh, n_seeds: int) -> list[Seed]:
+    """Generate seeds from a surface
+
+    The seeds are generated randomly over the triangles of the
+    surface. The orientation of each seed is opposite to the normal
+    of the surface.
+
+    It is assumed the ordering of the triangle indices follows the
+    right-hand rule and the normals point outward.
+
+    Args:
+        surface: The surface from which to generate the seeds, for example the
+            lh.white from FreeSurfer.
+        n_seeds: The number of seeds to generate.
+
+    Return:
+        A list of `n_seeds` seeds suitable for tractography.
+
+    """
+
+    # First choose a triangle for every seed and generate a random point
+    # inside that triangle.
+    vertices, triangles = surface.vertices, surface.triangles
+    indices = np.random.randint(len(triangles), size=n_seeds)
+    barycentric = [_random_barycentric_coordinates() for _ in range(n_seeds)]
+    triangle_vertices = [vertices[triangles[i]] for i in indices]
+    locations = [np.dot(b, v) for v, b in zip(triangle_vertices, barycentric)]
+
+    # The orientations of the seeds are just triangle normals.
+    normals = _triangle_normals(vertices, triangles)
+    orientations = -normals[indices]
+
+    return [Seed(el, n) for el, n in zip(locations, orientations)]
+
+
+def to_array(seeds: list[Seed]) -> npt.NDArray:
+    """Split seeds into location and orientation"""
+    return np.array([np.hstack((s.location, s.orientation)) for s in seeds])
+
+
+def save(filename: Path, seeds: list[Seed]):
+    """Save a list of seeds to file"""
+    data = [np.hstack((s.location, s.orientation)) for s in seeds]
+    np.savetxt(filename, data)
+
+
+def load(filename: Path) -> list[Seed]:
+    """Load a list of seed from a file"""
+    data = np.loadtxt(filename)
+    return [Seed(d[:3], d[3:]) for d in data]
+
+
+def _triangle_normals(vs, ts):
+    """Compute the normal of triangles"""
+    ns = [np.cross(vs[t[1]] - vs[t[0]], vs[t[2]] - vs[t[0]]) for t in ts]
+    return np.array([n / w if (w := np.linalg.norm(n)) != 0 else n for n in ns])
+
+
+def _random_barycentric_coordinates():
+    """Generate a random point in a triangle"""
+    a, b = np.random.rand(2)
+    if a + b > 1:
+        a = 1 - a
+        b = 1 - b
+    return 1 - a - b, a, b
