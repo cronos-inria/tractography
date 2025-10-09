03aee49c655eacf66718ce52fad210f3cd58b726
Samuel Deslauriers-Gauthier <samuel.deslauriers-gauthier@inria.fr>
2025-10-09 10:58:03 +0200

    test: use the new data generation functions to test diffusion
    This commit makes use of the new data generation functions for
    uniform and crossing fOD fields to test the diffusion algorithm.
    It also adds tests for the CLI using the same function.
    The previous data generation function are no longer needed and
    are removed.

--- test/test_algorithms/test_diffusion.py
+++ test/test_algorithms/test_diffusion.py
@@ -5,39 +5,60 @@ import nibabel as nib
 import numpy as np
 
 import tractography as tg
-import test.test_algorithms
+import test
 
 
 _OPENCL_DIR = Path(__file__).parents[2] / "src"
-_DATA_DIR = Path(__file__).parents[1] / "data"
+_TEST_RESULTS_DIR = (
+    Path(__file__).parents[2] / "test-results" / "algorithms" / "diffusion"
+)
 
 
 class TestDiffusion(unittest.TestCase):
     """Test the OpenCL implementation of Diffusion tractography"""
 
     def setUp(self):
-        test.test_algorithms.generate_cross_test_data()
+        _TEST_RESULTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    def test_uniform_isotropic(self):
+        """Test diffusion tractography on a uniform isotropic fOD field"""
+
+        # Prepare the data.
+        fod = test.data.uniform_isotropic()
+        affine = np.eye(4)
+        seeds = tg.seeds.from_odf(fod, affine, 1000)
+        nib.save(nib.Nifti1Image(fod, affine), _TEST_RESULTS_DIR / "uniform-fod.nii.gz")
+
+        # Generate the tractogram.
+        config = tg.configuration.load()
+        algorithm = tg.algorithms.Diffusion(fod, affine, len(seeds), config)
+        streamlines = algorithm.run(seeds)
+
+        # Save the streamlines for QA.
+        tractogram = nib.streamlines.Tractogram(streamlines, affine_to_rasmm=np.eye(4))
+        tck = nib.streamlines.TckFile(tractogram)
+        tck.save(_TEST_RESULTS_DIR / "uniform-streamlines.tck")
 
     def test_cross_tractography(self):
         """Test tractography on the cross dataset"""
 
-        fod = nib.load(_DATA_DIR / "cross-fod.nii.gz")
-        wm_mask = nib.load(_DATA_DIR / "cross-wm.nii.gz")
-        seed_mask = nib.load(_DATA_DIR / "cross-seed.nii.gz")
-        seeds = tg.seeds.from_mask(seed_mask.get_fdata(), seed_mask.affine, 1000)
-
-        fod_data = tg.core.apply_mask(
-            fod.get_fdata(), fod.affine, wm_mask.get_fdata(), wm_mask.affine
+        # Prepare the data.
+        fod = test.data.cross()
+        affine = np.eye(4)
+        nib.save(nib.Nifti1Image(fod, affine), _TEST_RESULTS_DIR / "cross-fod.nii.gz")
+        wm = fod[..., 0] > 0
+        nib.save(
+            nib.Nifti1Image(wm.astype(np.uint8), affine),
+            _TEST_RESULTS_DIR / "cross-wm.nii.gz",
         )
-        fod_data = tg.utils.normalize_odf(fod_data)
+        seeds = tg.seeds.from_mask(wm, affine, 1000)
 
+        # Generate the tractogram.
         config = tg.configuration.load()
-        config.algorithms.diffusion.save_at = 0.1
-        config.streamline.length.minimum = 1
-        config.streamline.length.maximum = 20
-        algorithm = tg.algorithms.Diffusion(fod_data, fod.affine, len(seeds), config)
+        algorithm = tg.algorithms.Diffusion(fod, affine, len(seeds), config)
         streamlines = algorithm.run(seeds)
 
+        # Save the streamlines for QA.
         tractogram = nib.streamlines.Tractogram(streamlines, affine_to_rasmm=np.eye(4))
         tck = nib.streamlines.TckFile(tractogram)
-        tck.save(_DATA_DIR / "cross-streamlines-diffusion.tck")
+        tck.save(_TEST_RESULTS_DIR / "cross-streamlines.tck")
---
+++ test/test_cli/test_tractogram.py
@@ -0,0 +1,41 @@
+import unittest
+from pathlib import Path
+
+import nibabel as nib
+import numpy as np
+
+import tractography as tg
+import tractography.cli.tractogram
+import test
+
+
+_TEST_RESULTS_DIR = Path(__file__).parents[2] / "test-results" / "cli" / "tractogram"
+
+
+class TestTractogram(unittest.TestCase):
+    """Test the generation of tractograms from the CLI"""
+
+    def setUp(self):
+        _TEST_RESULTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    def test_cross_tractography(self):
+        """Test tractography on the cross dataset"""
+
+        # Prepare the data.
+        fod = test.data.cross()
+        affine = np.eye(4)
+        fod_path = _TEST_RESULTS_DIR / "cross-fod.nii.gz"
+        nib.save(nib.Nifti1Image(fod, affine), fod_path)
+
+        wm = fod[..., 0] > 0
+        wm_path = _TEST_RESULTS_DIR / "cross-wm.nii.gz"
+        nib.save(nib.Nifti1Image(wm.astype(np.uint8), affine), wm_path)
+
+        seeds = tg.seeds.from_mask(wm, affine, 50000)
+        seeds_path = _TEST_RESULTS_DIR / "cross-seeds.txt"
+        tg.seeds.save(seeds_path, seeds)
+
+        tractogram_path = _TEST_RESULTS_DIR / "cross-streamlines.tck"
+        tg.cli.tractogram.main(
+            tg.Algorithm.DIFFUSION, fod_path, seeds_path, tractogram_path
+        )
